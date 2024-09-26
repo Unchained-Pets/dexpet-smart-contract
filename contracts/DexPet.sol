1ade94699e6e96407d7fec44e3ef3e03d1cb65ef
Ezeozue Chiagoziem <ezeozuechiagoziem@gmail.com>
2024-09-26 14:21:54 +0100

    Update on user getUnadoptedPets getAdoptedPets, getPetListing GetPetBids, GetUserBidsLength, Get Different Bid Status

--- contracts/DexPet.sol
+++ contracts/DexPet.sol
@@ -15,8 +15,9 @@ contract DexPet is ERC721, ERC721URIStorage {
 
     enum BidStatus {
         Open,
-        Closed,
-        Cancelled
+        Accepted,
+        Cancelled,
+        Rejected
     }
 
     constructor() ERC721("DexPet", "DPET") {
@@ -122,7 +123,7 @@ contract DexPet is ERC721, ERC721URIStorage {
         uint256 _petId,
         uint256 _startingPrice,
         uint256 _auctionDuration
-    ) public onlyOwner {
+    ) external onlyOwner {
         require(
             petIdToPets[_petId].isAdopted == false,
             "Pet is already adopted"
@@ -145,18 +146,17 @@ contract DexPet is ERC721, ERC721URIStorage {
         emit AuctionCreated(_petId, _startingPrice, endTime);
     }
 
-    function placeBid(uint256 _petId) public payable {
+    function placeBid(uint256 _petId) external payable {
         Auction storage auction = petIdToAuction[_petId];
+        require(auction.petId != 0, "Auction does not exist");
+        require(msg.sender != address(0), "Invalid sender address");
         require(auction.isActive, "Auction is not active");
         require(
             petIdToPets[_petId].isAdopted == false,
             "Pet is already adopted"
         );
         require(block.timestamp < auction.endTime, "Auction has ended");
-        require(
-            petIdToUserBids[_petId][msg.sender].length == 0,
-            "You have already bid on this pet"
-        );
+
         require(
             msg.value > auction.highestBid,
             "Bid must be higher than current highest bid"
@@ -170,18 +170,42 @@ contract DexPet is ERC721, ERC721URIStorage {
             require(success, "Failed to refund previous highest bidder");
         }
 
+        //Re-enter Bid if user has cancelled previous bid
+
+        // require(
+        //     petIdToUserBids[_petId][msg.sender].length == 0,
+        //     "You have already bid on this pet"
+        // );
+
+        // Check if the user has any open bids for this pet. Allow user to re-enter bid if they have cancelled previous bid
+        bool hasOpenBid = false;
+        uint256 userBidsLength = petIdToUserBids[_petId][msg.sender].length;
+        for (uint256 i = 0; i < userBidsLength; i++) {
+            if (
+                petIdToUserBids[_petId][msg.sender][i].bid_status ==
+                BidStatus.Open
+            ) {
+                hasOpenBid = true;
+                break;
+            }
+        }
+        require(!hasOpenBid, "You already have an open bid for this pet");
+
         //Generate a unique bid ID for the user
         uint256 userBidId = userToBids[msg.sender].length + 1;
-        Bid storage bid = userToBids[msg.sender][userBidId];
-        bid.bidId = userBidId;
-        bid.petId = _petId;
-        bid.amountBidded = msg.value;
-        bid.bidder = msg.sender;
-        bid.bid_status = BidStatus.Open;
-        userToBids[msg.sender].push(bid);
 
-        petIdToUserBids[_petId][msg.sender].push(bid);
-        petIdToBids[_petId].push(bid);
+        Bid memory newBid = Bid({
+            bidId: userBidId,
+            petId: _petId,
+            amountBidded: msg.value,
+            bidder: msg.sender,
+            bid_status: BidStatus.Open
+        });
+
+        userToBids[msg.sender].push(newBid);
+
+        petIdToUserBids[_petId][msg.sender].push(newBid);
+        petIdToBids[_petId].push(newBid);
 
         //Increment total bids
         totalBids++;
@@ -192,7 +216,9 @@ contract DexPet is ERC721, ERC721URIStorage {
         emit BidPlaced(_petId, msg.sender, msg.value);
     }
 
-    function cancelBid(uint256 _petId, uint256 _bidId) public {
+    function cancelBid(uint256 _petId, uint256 _bidId) external {
+        require(msg.sender != address(0), "Invalid sender address");
+        require(petIdToAuction[_petId].isActive, "Auction is not active");
         require(
             petIdToUserBids[_petId][msg.sender].length > 0,
             "You have not bid on this pet"
@@ -202,22 +228,29 @@ contract DexPet is ERC721, ERC721URIStorage {
                 BidStatus.Open,
             "Your bid is not open"
         );
-        petIdToUserBids[_petId][msg.sender][_bidId].bid_status = BidStatus
-            .Cancelled;
+
+        Bid storage bid = petIdToUserBids[_petId][msg.sender][_bidId];
+
+        //Check if user is the higest bidder at this point
+        Auction memory auction = petIdToAuction[_petId];
+        if (auction.highestBidder == msg.sender) {
+            auction.highestBid = 0;
+            auction.highestBidder = address(0);
+        }
+
+        bid.bid_status = BidStatus.Cancelled;
+        userToBids[msg.sender][_bidId].bid_status = BidStatus.Cancelled;
 
         //Refund the bid amount to the bidder
-        (bool success, ) = payable(msg.sender).call{
-            value: petIdToUserBids[_petId][msg.sender][_bidId].amountBidded
-        }("");
-        require(success, "Failed to refund previous highest bidder");
-        emit BidCancelled(
-            _petId,
-            msg.sender,
-            petIdToUserBids[_petId][msg.sender][_bidId].amountBidded
+        (bool success, ) = payable(msg.sender).call{value: bid.amountBidded}(
+            ""
         );
+        require(success, "Failed to refund previous highest bidder");
+
+        emit BidCancelled(_petId, msg.sender, bid.amountBidded);
     }
 
-    function endAuction(uint256 _petId) public onlyOwner {
+    function endAuction(uint256 _petId, uint256 _bidId) external onlyOwner {
         Auction storage auction = petIdToAuction[_petId];
         require(auction.isActive, "Auction is not active");
         require(
@@ -225,22 +258,42 @@ contract DexPet is ERC721, ERC721URIStorage {
             "Auction has not ended yet"
         );
 
-        auction.isActive = false;
-        Pet storage pet = petIdToPets[_petId];
-        pet.isAdopted = true;
-        pet.price = auction.highestBid;
-        pet.adopter = auction.highestBidder;
-
         if (auction.highestBidder != address(0)) {
-            // Transfer the pet to the highest bidder
-            // You might want to implement a transfer function or update ownership here
+            Pet storage pet = petIdToPets[_petId];
+            pet.isAdopted = true;
+            pet.price = auction.highestBid;
+            pet.adopter = auction.highestBidder;
+
+            // Update the bid status to accepted
+            Bid storage bid = petIdToUserBids[_petId][auction.highestBidder][
+                _bidId
+            ];
+            auction.isActive = false;
+            bid.bid_status = BidStatus.Accepted;
+            userToBids[auction.highestBidder][_bidId].bid_status = BidStatus.Accepted;
+
+            // Set other bids to rejected
+            for (uint256 i = 0; i < petIdToBids[_petId].length; i++) {
+                Bid storage petBid = petIdToBids[_petId][i];
+                Bid storage UserPet = userToBids[auction.highestBidder][i];
+                if (
+                    petBid.bidder != auction.highestBidder &&
+                    petBid.bid_status == BidStatus.Open
+                ) {
+                    petBid.bid_status = BidStatus.Rejected;
+                    UserPet.bid_status = BidStatus.Rejected;
+                }
+            }
+
             (bool success, ) = payable(auction.highestBidder).call{
                 value: auction.highestBid
             }("");
             require(success, "Failed to refund previous highest bidder");
+
             // Mint NFT to the highest bidder
             _safeMint(auction.highestBidder, _petId);
             _setTokenURI(_petId, pet.picture);
+
             emit NFTMinted(_petId, auction.highestBidder, pet.petId);
             emit AuctionEnded(
                 _petId,
@@ -252,6 +305,142 @@ contract DexPet is ERC721, ERC721URIStorage {
         }
     }
 
+    //For Pet Lovers
+    function getUnadoptedPets() external view returns (Pet[] memory) {
+        Pet[] memory _petListings = new Pet[](petId);
+        uint256 count = 0;
+        for (uint256 i = 0; i < petId; i++) {
+            if (petIdToPets[i + 1].isAdopted == false) {
+                _petListings[count] = petIdToPets[i + 1];
+                count++;
+            }
+        }
+        return _petListings;
+    }
+
+    function getAdoptedPets() external view returns (Pet[] memory) {
+        Pet[] memory _petListings = new Pet[](petId);
+        uint256 count = 0;
+        for (uint256 i = 0; i < petId; i++) {
+            if (petIdToPets[i + 1].isAdopted == true) {
+                _petListings[count] = petIdToPets[i + 1];
+                count++;
+            }
+        }
+        return _petListings;
+    }
+
+    function getPetListing(uint256 _petId) external view returns (Pet memory) {
+        return petIdToPets[_petId];
+    }
+
+    function getPetBids(uint256 _petId) external view returns (Bid[] memory) {
+        return petIdToBids[_petId];
+    }
+
+    function getUserBids(
+        uint256 _petId,
+        address _user
+    ) external view returns (Bid[] memory) {
+        return petIdToUserBids[_petId][_user];
+    }
+
+    function getUserBidsLength(address _user) external view returns (uint256) {
+        return userToBids[_user].length;
+    }
+
+    function getUserOpenBids(
+        address _user
+    ) external view returns (Bid[] memory) {
+        uint256 openBidsCount = 0;
+        for (uint256 i = 0; i < userToBids[_user].length; i++) {
+            if (userToBids[_user][i].bid_status == BidStatus.Open) {
+                openBidsCount++;
+            }
+        }
+
+        Bid[] memory _openBids = new Bid[](openBidsCount);
+
+        uint256 index = 0;
+        for (uint256 i = 0; i < userToBids[_user].length; i++) {
+            if (userToBids[_user][i].bid_status == BidStatus.Open) {
+                _openBids[index] = userToBids[_user][i];
+                index++;
+            }
+        }
+
+        return _openBids;
+    }
+
+    function getUserCancelledBids(
+        address _user
+    ) external view returns (Bid[] memory) {
+        uint256 cancelledBidsCount = 0;
+        for (uint256 i = 0; i < userToBids[_user].length; i++) {
+            if (userToBids[_user][i].bid_status == BidStatus.Cancelled) {
+                cancelledBidsCount++;
+            }
+        }
+
+        Bid[] memory _cancelledBids = new Bid[](cancelledBidsCount);
+
+        uint256 index = 0;
+        for (uint256 i = 0; i < userToBids[_user].length; i++) {
+            if (userToBids[_user][i].bid_status == BidStatus.Cancelled) {
+                _cancelledBids[index] = userToBids[_user][i];
+                index++;
+            }
+        }
+
+        return _cancelledBids;
+    }
+
+    function getUserRejectedBids(
+        address _user
+    ) external view returns (Bid[] memory) {
+        uint256 rejectedBidsCount = 0;
+        for (uint256 i = 0; i < userToBids[_user].length; i++) {
+            if (userToBids[_user][i].bid_status == BidStatus.Rejected) {
+                rejectedBidsCount++;
+            }
+        }
+
+        Bid[] memory _rejectedBids = new Bid[](rejectedBidsCount);
+
+        uint256 index = 0;
+        for (uint256 i = 0; i < userToBids[_user].length; i++) {
+            if (userToBids[_user][i].bid_status == BidStatus.Rejected) {
+                _rejectedBids[index] = userToBids[_user][i];
+                index++;
+            }
+        }
+
+        return _rejectedBids;
+    }
+
+    function getUserAcceptedBids(
+        address _user
+    ) external view returns (Bid[] memory) {
+        uint256 acceptedBidsCount = 0;
+        for (uint256 i = 0; i < userToBids[_user].length; i++) {
+            if (userToBids[_user][i].bid_status == BidStatus.Accepted) {
+                acceptedBidsCount++;
+            }
+        }
+
+        Bid[] memory _acceptedBids = new Bid[](acceptedBidsCount);
+
+        uint256 index = 0;
+        for (uint256 i = 0; i < userToBids[_user].length; i++) {
+            if (userToBids[_user][i].bid_status == BidStatus.Accepted) {
+                _acceptedBids[index] = userToBids[_user][i];
+                index++;
+            }
+        }
+
+        return _acceptedBids;
+    }
+    
     modifier onlyOwner() {
         require(msg.sender == owner, "Only the owner can perform this action");
         _;
